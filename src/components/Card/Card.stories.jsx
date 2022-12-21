c96d20df9e2418e8cafebbbc436aae098fcf00f2
Ruben Mesta <rubenmesta29@gmail.com>
2022-12-21 11:23:15 -0500

    fix storybook

--- src/components/Card/Card.stories.jsx
+++ src/components/Card/Card.stories.jsx
@@ -13,22 +13,34 @@ const Template = (args) => <Card {...args} />;
 export const International = Template.bind({});
 International.args = {
   destination: "International",
-  src: "/images/london-img.jpg",
-  eyebrow: "International",
-  city: "Paris, France",
-  date: "12/19/2022",
-  description: "Torquatos nostros? quos tu paulo ante cum soluta nobis est",
+  image: {
+    src: "/images/london-img.jpg",
+    alt: "alt text",
+  },
+  details: {
+    eyebrow: "Register Now",
+    city: "Paris, France",
+    date: "12/19/2022",
+    description: "Torquatos nostros? quos tu paulo ante cum soluta nobis est",
+    buttonText: "Plan your trip",
+  },
   className: "Card",
-  width: 500,
 };
 
 export const Domestic = Template.bind({});
 Domestic.args = {
   destination: "Domestic",
-  src: "/images/atlanta-img.png",
-  eyebrow: "Domestic",
-  city: "Denver, CO",
-  date: "12/19/2022",
-  description: "Torquatos nostros? quos tu paulo ante cum soluta nobis est",
+  image: {
+    src: "/images/atlanta-img.png",
+    alt: "alt text",
+  },
+  details: {
+    eyebrow: "Register Now",
+    city: "Denver, CO",
+    date: "12/19/2022",
+    description: "Torquatos nostros? quos tu paulo ante cum soluta nobis est",
+    buttonText: "cta text",
+  },
+
   className: "Card",
 };
--- src/stories/Home.stories.jsx
+++ src/stories/Home.stories.jsx
@@ -16,32 +16,38 @@ export const Grid = (args) => (
     <section className="wrapper" style={{ maxWidth: "100%" }}>
       <Card
         destination="International"
-        src="/images/london-img.jpg"
-        alt=""
-        eyebrow="Register Now"
-        city="Paris, France"
-        date="12/19/2022"
-        description="Torquatos nostros? quos tu paulo ante cum soluta nobis est"
+        image={{ src: "/images/london-img.jpg", alt: "" }}
+        details={{
+          eyebrow: "Register Now",
+          city: "Paris, France",
+          date: "12/19/2022",
+          description:
+            "Torquatos nostros? quos tu paulo ante cum soluta nobis est",
+        }}
         className="Card first"
       />
       <Card
         destination="Domestic"
-        src="/images/london-img.jpg"
-        alt=""
-        eyebrow="Register Now"
-        city="Atlanta, GA"
-        date="12/19/2022"
-        description="Torquatos nostros? quos tu paulo ante cum soluta nobis est"
+        image={{ src: "/images/london-img.jpg", alt: "" }}
+        details={{
+          eyebrow: "Register Now",
+          city: "Atlanta, GA",
+          date: "12/19/2022",
+          description:
+            "Torquatos nostros? quos tu paulo ante cum soluta nobis est",
+        }}
         className="Card second"
       />
       <Card
         destination="international"
-        src="/images/london-img.jpg"
-        alt=""
-        eyebrow="3 Spots Left!"
-        city="Singapore"
-        date="12/19/2022"
-        description="Torquatos nostros? quos tu paulo ante cum soluta nobis est"
+        image={{ src: "/images/london-img.jpg", alt: "" }}
+        details={{
+          eyebrow: "3 Spots Left!",
+          city: "Singapore",
+          date: "12/19/2022",
+          description:
+            "Torquatos nostros? quos tu paulo ante cum soluta nobis est",
+        }}
         className="Card third"
       />
     </section>
